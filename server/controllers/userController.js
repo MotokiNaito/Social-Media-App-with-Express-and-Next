ba21f7da528949c067b46afb604521e59c991426
Motoki Naito <motoki.developer@gmail.com>
2019-06-20 08:31:27 -0700

    add getUser function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,11 +6,40 @@ exports.getUsers = async (req, res) => {
   res.json(users);
 };
 
-exports.getAuthUser = () => {};
+exports.getAuthUser = (req, res) => {
+  if (!req.isAuthUser) {
+    res.status(403).json({
+      message: 'You are unauthenticated. Please sign in or sign up'
+    })
+    return res.redirect('/signin')
+  }
+  res.json(req.user)
+};
 
-exports.getUserById = () => {};
+exports.getUserById = async (req, res, next, id) => {
+  const user = await User.findOne({
+    _id: id
+  });
+  req.profile = user;
 
-exports.getUserProfile = () => {};
+  const profileId = mongooose.Types.ObjectId(req.profile._id)
+
+  if (profileId.equals(req.user._id)) {
+    req.isAuthUser = true;
+    return next();
+  }
+  next();
+};
+
+exports.getUserProfile = (req, res) => {
+  if (!req.profile) {
+    return res.status(404).json({
+      message: 'No user found'
+    })
+  }
+
+  res.json(req.profile)
+};
 
 exports.getUserFeed = () => {};
 
@@ -20,7 +49,22 @@ exports.resizeAvatar = () => {};
 
 exports.updateUser = () => {};
 
-exports.deleteUser = () => {};
+exports.deleteUser = async (req, res) => {
+  const {
+    userId
+  } = req.params;
+
+  if (!req.isAuthUser) {
+    return res.status(400).json({
+      message: 'You are not authorized to perform this action'
+    })
+  }
+
+  const deletedUser = await User.findOneAndDelete({
+    _id: userId
+  })
+  res.json(deletedUser);
+};
 
 exports.addFollowing = () => {};
 
