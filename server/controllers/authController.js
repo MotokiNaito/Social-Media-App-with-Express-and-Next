ee181537a8e7b526ab1303aa20e519236d5c8faa
Motoki Naito <motoki.developer@gmail.com>
2019-06-19 21:24:40 -0700

    add authcontroller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,91 @@
-exports.validateSignup = () => {};
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const passport = require('passport');
 
-exports.signup = () => {};
+exports.validateSignup = (req, res, next) => {
+  req.sanitizeBody('name');
+  req.sanitizeBody('email');
+  req.sanitizeBody('password');
 
-exports.signin = () => {};
+  req.checkBody('name', 'Enter a name').notEmpty();
+  req.checkBody('name', 'Name must be between 4 and 10 characters')
+    .isLength({
+      min: 4,
+      max: 10
+    })
 
-exports.signout = () => {};
+  req.checkBody('email', 'Enter a valid email')
+    .isEmail()
+    .normalizeEmail()
 
-exports.checkAuth = () => {};
+  req.checkBody('password', 'Enter a password').notEmpty();
+  req.checkBody('password', 'Password must be between 4 and 10 characters')
+    .isLength({
+      min: 4,
+      max: 10
+    })
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    const firstError = errors.map(error => error.msg)[0];
+    return res.status(400).send(firstError);
+  }
+  next();
+};
+
+exports.signup = async (req, res) => {
+  const {
+    name,
+    email,
+    password
+  } = req.body;
+
+  const user = await new User({
+    name,
+    email,
+    password
+  });
+
+  await User.register(user, password, (err, user) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.json(user.name);
+  });
+
+};
+
+exports.signin = (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return res.status(500).json(err.message);
+    }
+    if (!user) {
+      return res.status(400).json(info.message)
+    }
+
+    req.logIn(user, err => {
+      if (err) {
+        return res.status(500).json(err.message)
+      }
+
+      res.json(user)
+    });
+  })(req, res, next);
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie('next-cookie.sid');
+  req.logout();
+  res.json({
+    message: 'You are now signed out!'
+  });
+};
+
+exports.checkAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/signin');
+};
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,10 @@
-exports.getUsers = () => {};
+const mongooose = require('mongoose');
+const User = mongooose.model('User');
+
+exports.getUsers = async (req, res) => {
+  const users = await User.find().select("_id name email createdAt updatedAt");
+  res.json(users);
+};
 
 exports.getAuthUser = () => {};
 
