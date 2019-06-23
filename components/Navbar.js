d1d9472c4f3bbb990c3b96afdd415abbf3d0eb73
Motoki Naito <motoki.developer@gmail.com>
2019-06-23 11:25:02 -0700

    add signup functions

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,51 @@
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import ShareOutlined from "@material-ui/icons/ShareOutlined";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ShareOutlined from "@material-ui/icons/ShareOutlined";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+import ActiveLink from "./ActiveLink";
+import { signoutUser } from "../lib/auth";
+
+const Navbar = ({ classes, router, pageProps: { auth } }) => {
+  const { user = {} } = auth || {};
+
+  return (
+    <AppBar className={classes.appBar} position={router.pathname === "/" ? "fixed" : "static"}>
+      <Toolbar>
+        {/* Main Title / Home Button */}
+        <ActiveLink href="/">
+          <ShareOutlined className={classes.icon} />
+        </ActiveLink>
+        <Typography variant="h5" component="h1" className={classes.toolbarTitle}>
+          <ActiveLink href="/">NextConnect</ActiveLink>
+        </Typography>
+
+        {user._id ? (
+          // Auth Navigation
+          <div>
+            <Button>
+              <ActiveLink href={`/profile/${user._id}`}>Profile</ActiveLink>
+            </Button>
+            <Button onClick={signoutUser} variant="outlined">
+              Sign out
+            </Button>
+          </div>
+        ) : (
+          // UnAuth Navigation
+          <div>
+            <Button>
+              <ActiveLink href="/signin">Sign in</ActiveLink>
+            </Button>
+            <Button>
+              <ActiveLink href="/signup">Sign up</ActiveLink>
+            </Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 const styles = theme => ({
--- pages/signup.js
+++ pages/signup.js
@@ -1,26 +1,135 @@
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import FormControl from "@material-ui/core/FormControl";
-// import Paper from "@material-ui/core/Paper";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Button from "@material-ui/core/Button";
-// import Snackbar from "@material-ui/core/Snackbar";
-// import Dialog from "@material-ui/core/Dialog";
-// import DialogActions from "@material-ui/core/DialogActions";
-// import DialogContent from "@material-ui/core/DialogContent";
-// import DialogContentText from "@material-ui/core/DialogContentText";
-// import DialogTitle from "@material-ui/core/DialogTitle";
-// import Slide from "@material-ui/core/Slide";
-// import Gavel from "@material-ui/icons/Gavel";
-// import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import Gavel from "@material-ui/icons/Gavel";
+import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
 import withStyles from "@material-ui/core/styles/withStyles";
+import Link from "next/link";
+
+import { signupUser } from "../lib/auth";
+
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
 
 class Signup extends React.Component {
-  state = {};
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    createdUser: "",
+    openError: false,
+    openSuccess: false,
+    isLoading: false
+  };
+
+  handleClose = () => this.setState({ openError: false });
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    const { name, email, password } = this.state;
+
+    event.preventDefault();
+    const user = { name, email, password };
+    this.setState({ isLoading: true, error: "" });
+    signupUser(user)
+      .then(createdUser => {
+        this.setState({
+          createdUser,
+          error: "",
+          openSuccess: true,
+          isLoading: false
+        });
+      })
+      .catch(this.showError);
+  };
+
+  showError = err => {
+    const error = (err.response && err.response.data) || err.message;
+    this.setState({ error, openError: true, isLoading: false });
+  };
 
   render() {
-    return <div>Signup</div>;
+    const { classes } = this.props;
+    const { error, openError, openSuccess, createdUser, isLoading } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <Gavel />
+          </Avatar>
+          <Typography variant="h5" component="h1">
+            Sign up
+          </Typography>
+
+          <form onSubmit={this.handleSubmit} className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="name">Name</InputLabel>
+              <Input name="name" type="text" onChange={this.handleChange} />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email</InputLabel>
+              <Input name="email" type="email" onChange={this.handleChange} />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input name="password" type="password" onChange={this.handleChange} />
+            </FormControl>
+            <Button type="submit" fullWidth variant="contained" color="primary" disabled={isLoading} className={classes.submit}>
+              {isLoading ? "Signing up..." : "Sign up"}
+            </Button>
+          </form>
+
+          {/* Error Snackbar */}
+          {error && (
+            <Snackbar
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right"
+              }}
+              open={openError}
+              onClose={this.handleClose}
+              autoHideDuration={6000}
+              message={<span className={classes.snack}>{error}</span>}
+            />
+          )}
+        </Paper>
+
+        {/* Success Dialog */}
+        <Dialog open={openSuccess} disableBackdropClick={true} TransitionComponent={Transition}>
+          <DialogTitle>
+            <VerifiedUserTwoTone className={classes.icon} />
+            New Account
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>User {createdUser} successfully created!</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" variant="contained">
+              <Link href="/signin">
+                <a className={classes.signinLink}>Sign in</a>
+              </Link>
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
   }
 }
 
