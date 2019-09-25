a330b694808c7dcdcb2a04b2950f9f0917d900eb
Motoki Naito <motoki.developer@gmail.com>
2019-09-24 21:07:18 -0700

    updating new-post components and README.md

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # About
-![logo](/static/images/next-connect-logo-blue.svg)
 
-Socail Media App using Next.js and Express.js
+![logo](/static/images/next-connect.svg)
+
+This is a Socail Media App like Facebook.<br>
 (https://next-connect-app.herokuapp.com/)
 
-## Skills I Used
+# Skills I used
 
-1. React/Next.js
-2. Express.js/Node.js
-3. MongoDB
-4. Material UI
+![logo](/static/images/skills.png)
 
-## Getting Started
+- React
+- Next.js
+- Node.js
+- Express
+- Passport
+- MongoDB
 
-1. Clone this repo with `git clone git@github.com:MotokiNaito/Social-Media-App-with-Express-and-Next.git`
-2. `npm install` or `yarn`
-3. Go to [MLab](mlab.com) and create a new database
-4. Copy the Database URI to connect using the Mongo Driver
-5. Change your .env.default file to just .env and paste the copied uri as the value for MONGO_URI in .env
-6. Add any random string for the SESSION_SECRET entry in your .env file
-7. Run with `npm run dev` or `yarn dev`
-8. Server should be listening on localhost:3000
+# Why React?
+
+# SSR vs CSR
+
+# Upcoming Features
--- components/index/NewPost.js
+++ components/index/NewPost.js
@@ -1,78 +1,26 @@
-import Card from "@material-ui/core/Card";
-import Typography from "@material-ui/core/Typography";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import AddAPhoto from "@material-ui/icons/AddAPhoto";
-import withStyles from "@material-ui/core/styles/withStyles";
-
-const NewPost = ({ classes, auth, text, image, isAddingPost, handleChange, handleAddPost }) => (
-  <Card className={classes.card}>
-    <CardHeader
-      avatar={<Avatar src={auth.user.avatar} />}
-      title={
-        <Typography variant="h6" component="h2" className={classes.cardTitle}>
-          {auth.user.name}
-        </Typography>
-      }
-      className={classes.cardHeader}
-    />
-    <CardContent className={classes.cardContent}>
-      <TextField
-        label="Add a status"
-        value={text}
-        name="text"
-        multiline
-        row="2"
-        placeholder={`What's on your mind, ${auth.user.name}?`}
-        fullWidth
-        margin="normal"
-        onChange={handleChange}
-        variant="outlined"
-        InputLabelProps={{
-          shrink: true
-        }}
-      />
-      <input accept="image/*" name="image" id="image" onChange={handleChange} className={classes.input} type="file" />
+const NewPost = ({ auth, text, image, isAddingPost, handleChange, handleAddPost }) => (
+  <section className="new-post">
+    <div className="new-post__header">
+      <h2>Create a post</h2>
+      <div className="new-post__header__inner">
+        <div className="new-post__header__avatar">
+          <img src={auth.user.avatar} />
+        </div>
+        <h2>{auth.user.name}</h2>
+      </div>
+    </div>
+    <div className="new-post__content">
+      <input value={text} name="text" placeholder={`What's on your mind, ${auth.user.name}?`} onChange={handleChange} />
+      <input accept="image/*" name="image" id="image" onChange={handleChange} className="image-input" type="file" />
       <label htmlFor="image">
-        <IconButton color="secondary" component="span">
-          <AddAPhoto />
-        </IconButton>
+        <p>Add a photo</p>
       </label>
       <span>{image && image.name}</span>
-    </CardContent>
-    <CardActions className={classes.cardActions}>
-      <Button color="primary" variant="contained" disabled={!text || isAddingPost} className={classes.submit} onClick={handleAddPost}>
-        {isAddingPost ? "Sending" : "Post"}
-      </Button>
-    </CardActions>
-  </Card>
+    </div>
+    <button className="btn" disabled={!text || isAddingPost} onClick={handleAddPost}>
+      {isAddingPost ? "Sending" : "Post"}
+    </button>
+  </section>
 );
 
-const styles = theme => ({
-  card: {
-    marginBottom: theme.spacing.unit * 3,
-    backgroundColor: theme.palette.primary.main
-  },
-  cardTitle: {
-    color: "#fff"
-  },
-  cardContent: {
-    backgroundColor: "white"
-  },
-  input: {
-    display: "none"
-  },
-  cardActions: {
-    display: "flex",
-    flexDirection: "row-reverse",
-    backgroundColor: "white",
-    borderTop: "1px solid #ccc"
-  }
-});
-
-export default withStyles(styles)(NewPost);
+export default NewPost;
