c4356de9e311956cd40d4188a9aa9af9a5623fd9
Motoki Naito <motoki.developer@gmail.com>
2019-10-09 14:28:44 +0900

    updating edit profile component

--- components/profile/FollowTab.js
+++ components/profile/FollowTab.js
@@ -1,52 +1,20 @@
-import Avatar from "@material-ui/core/Avatar";
-import Typography from "@material-ui/core/Typography";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import withStyles from "@material-ui/core/styles/withStyles";
 import Link from "next/link";
 
-const FollowTab = ({ classes, users }) => (
-  <div className={classes.root}>
-    <GridList cellHeight={160} className={classes.gridList} cols={4}>
+const FollowTab = ({ users }) => (
+  <div className="follow-tab">
+    <ul className="follow-tab-lists">
       {users.map(user => (
-        <GridListTile style={{ height: 120 }} key={user._id}>
+        <li className="follow-tab-list" key={user._id}>
           <Link href={`/profile/${user._id}`}>
             <a>
-              <Avatar src={user.avatar} className={classes.bigAvatar} />
-              <Typography component="h3" variant="subtitle1" className={classes.tileText}>
-                {user.name}
-              </Typography>
+              <img src={user.avatar} className="follow-tab-list__avatar" />
+              <h4>{user.name}</h4>
             </a>
           </Link>
-        </GridListTile>
+        </li>
       ))}
-    </GridList>
+    </ul>
   </div>
 );
 
-const styles = theme => ({
-  root: {
-    marginTop: theme.spacing.unit * 2,
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden"
-  },
-  bigAvatar: {
-    width: 60,
-    height: 60,
-    margin: "auto"
-  },
-  gridList: {
-    width: 300,
-    [theme.breakpoints.up("sm")]: {
-      width: 400
-    }
-  },
-  tileText: {
-    textAlign: "center",
-    marginTop: 10
-  }
-});
-
-export default withStyles(styles)(FollowTab);
+export default FollowTab;
--- pages/edit-profile.js
+++ pages/edit-profile.js
@@ -1,21 +1,3 @@
-import Avatar from "@material-ui/core/Avatar";
-import FormControl from "@material-ui/core/FormControl";
-import Paper from "@material-ui/core/Paper";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import Snackbar from "@material-ui/core/Snackbar";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import CloudUpload from "@material-ui/icons/CloudUpload";
-import FaceTwoTone from "@material-ui/icons/FaceTwoTone";
-import EditSharp from "@material-ui/icons/EditSharp";
-import withStyles from "@material-ui/core/styles/withStyles";
 import Router from "next/router";
 
 import { authInitialProps } from "../lib/auth";
@@ -89,140 +71,41 @@ class EditProfile extends React.Component {
   };
 
   render() {
-    const { classes } = this.props;
     const { name, email, avatar, about, avatarPreview, isLoading, isSaving, updatedUser, openSuccess, openError, error } = this.state;
 
     return (
-      <div className={classes.root}>
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <EditSharp />
-          </Avatar>
-          <Typography variant="h5" component="h1">
-            Edit Profile
-          </Typography>
+      <div className="edit-profile">
+        <h1>Edit Profile</h1>
 
-          {/* Edit Profile Form */}
-          <form onSubmit={this.handleSubmit} className={classes.form}>
-            {isLoading ? (
-              <Avatar className={classes.bigAvatar}>
-                <FaceTwoTone />
-              </Avatar>
-            ) : (
-              <Avatar src={avatarPreview || avatar} className={classes.bigAvatar} />
-            )}
-            <input type="file" name="avatar" id="avatar" accept="image/*" onChange={this.handleChange} className={classes.input} />
-            <label htmlFor="avatar" className={classes.uploadButton}>
-              <Button variant="contained" color="secondary" component="span">
-                Upload Image <CloudUpload />
-              </Button>
-            </label>
-            <span className={classes.filename}>{avatar && avatar.name}</span>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="name">Name</InputLabel>
-              <Input type="text" name="name" value={name} onChange={this.handleChange} />
-            </FormControl>
-            <FormControl margin="normal" fullWidth>
-              <InputLabel htmlFor="about">About</InputLabel>
-              <Input type="text" name="about" value={about} onChange={this.handleChange} />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">Email</InputLabel>
-              <Input type="email" name="email" value={email} onChange={this.handleChange} />
-            </FormControl>
-            <Button type="submit" fullWidth disabled={isSaving || isLoading} variant="contained" color="primary" className={classes.submit}>
-              {isSaving ? "Saving..." : "Save"}
-            </Button>
-          </form>
-        </Paper>
+        <form onSubmit={this.handleSubmit}>
+          {isLoading ? <i className="material-icons">face</i> : <img src={avatarPreview || avatar} />}
+          <input className="input-file" type="file" name="avatar" id="avatar" accept="image/*" onChange={this.handleChange} />
+          <label htmlFor="avatar">
+            <button className="btn">
+              Upload Image <i class="material-icons">cloud_upload</i>
+            </button>
+          </label>
+          <span>{avatar && avatar.name}</span>
+          <input placeholder="Name" type="text" name="name" value={name} onChange={this.handleChange} />
+          <input placeholder="About" type="text" name="about" value={about} onChange={this.handleChange} />
+          <input placeholder="Email" type="email" name="email" value={email} onChange={this.handleChange} />
 
-        {/* Error Snackbar */}
-        {error && (
-          <Snackbar
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "right"
-            }}
-            open={openError}
-            onClose={this.handleClose}
-            autoHideDuration={6000}
-            message={<span className={classes.snack}>{error}</span>}
-          />
+          <button className="btn" type="submit" disabled={isSaving || isLoading}>
+            {isSaving ? "Saving..." : "Save"}
+          </button>
+        </form>
+        {error && <span className="edit-profile__error">{error}</span>}
+        {openSuccess && (
+          <div className="edit-profile__success">
+            <h4>Profile Updated</h4>
+            <p>User {updatedUser && updatedUser.name} was successfully updated!</p>
+          </div>
         )}
-
-        {/* Success Dialog */}
-        <Dialog open={openSuccess} disableBackdropClick={true}>
-          <DialogTitle>
-            <VerifiedUserTwoTone className={classes.icon} />
-            Profile Updated
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText>User {updatedUser && updatedUser.name} was successfully updated!</DialogContentText>
-          </DialogContent>
-        </Dialog>
       </div>
     );
   }
 }
 
-const styles = theme => ({
-  root: {
-    width: "auto",
-    display: "block",
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up("md")]: {
-      width: 400,
-      marginLeft: "auto",
-      marginRight: "auto"
-    }
-  },
-  bigAvatar: {
-    width: 60,
-    height: 60,
-    margin: "auto"
-  },
-  uploadButton: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    margin: "0.25em"
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: theme.spacing.unit * 2
-  },
-  signinLink: {
-    textDecoration: "none",
-    color: "white"
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%",
-    marginTop: theme.spacing.unit
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 2
-  },
-  snack: {
-    color: theme.palette.secondary.light
-  },
-  icon: {
-    padding: "0px 2px 2px 0px",
-    verticalAlign: "middle",
-    color: "green"
-  },
-  input: {
-    display: "none"
-  }
-});
-
 EditProfile.getInitialProps = authInitialProps(true);
 
-export default withStyles(styles)(EditProfile);
+export default EditProfile;
